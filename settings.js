8c005c2344a736a388d823388156bdea5c8825f6
Usbswu <jakabik2@gmail.com>
2025-06-24 09:23:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,6 +4,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+SESSION_ID: process.env.SESSION_ID || "7VdywLjI#cl2EIMRkJQWQ6J-xBfwd0euN2ZkBKOqu3uaR6pjFxyI",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
